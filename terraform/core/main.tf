530caa54488561f5c63b5fab9f7574c28f16c03f
pandawelch <19201714+pandawelch@users.noreply.github.com>
2022-06-07 09:43:28 +1000

    Shorten storage account name

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "SA" {
-  name                     = "wggroundupvpnstoragestorage"
+  name                     = "wggroundupvpnstorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
