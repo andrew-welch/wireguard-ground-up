266670826ca68e3d55f680bb476e82ed5b6a0ad0
pandawelch <19201714+pandawelch@users.noreply.github.com>
2022-06-07 09:41:20 +1000

    Remove bad characters from Storage Account name

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "SA" {
-  name                     = "WG-VPN-Storage"
+  name                     = "wggroundupvpnstoragestorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
