84c52868d2417e6bd5e5de58306113346edadc82
pandawelch <19201714+pandawelch@users.noreply.github.com>
2022-06-06 20:13:52 +1000

    commit core network content

---
+++ terraform/core/main.tf
@@ -0,0 +1,25 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.8.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_location
+  tags = {
+  	ManagedBy = "Terraform"
+  }
+}
---
+++ terraform/core/variables.tf
@@ -0,0 +1,17 @@
+variable "resource_location" {
+  default = "australiaeast"
+}
+
+variable "resource_group_name" {
+  default = "az-wireguard-ground-up"
+}
+
+
+
+variable "password" {
+  default = "a1b2c3d4e5f6G7"
+}
+
+variable "domain-name" {
+  default = "example.com"
+}
