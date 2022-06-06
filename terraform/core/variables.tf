2761548181b1a8b1d43553247ee787984a4cc9b7
pandawelch <19201714+pandawelch@users.noreply.github.com>
2022-06-06 20:18:21 +1000

    Remove variables

--- terraform/core/variables.tf
+++ terraform/core/variables.tf
@@ -5,13 +5,3 @@ variable "resource_location" {
 variable "resource_group_name" {
   default = "az-wireguard-ground-up"
 }
-
-
-
-variable "password" {
-  default = "a1b2c3d4e5f6G7"
-}
-
-variable "domain-name" {
-  default = "example.com"
-}
